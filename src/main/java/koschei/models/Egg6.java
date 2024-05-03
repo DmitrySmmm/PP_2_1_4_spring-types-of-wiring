119d987c5359c8502a320e351b46a695490066e3
DmitrySmmm <deein@bk.ru>
2024-05-04 01:44:15 +0300

    1 commit

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -3,7 +3,7 @@ package koschei.models;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Egg6 {
-    private Needle7 needle;
+    private final Needle7 needle;
     @Autowired
     public Egg6(Needle7 needle) {
         this.needle = needle;
